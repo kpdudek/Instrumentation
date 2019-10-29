c80d2edabc48b0addd9fa270c9e7f3175aae0089
Kurt <kpdudek2@gmail.com>
2019-10-29 09:17:19 -0400

    uncertainty finished

--- Lab4/Lab4.m
+++ Lab4/Lab4.m
@@ -93,68 +93,6 @@ analyze_data(ave_rtdData,std_rtdData,'RTD with Bridge')
 analyze_data(ave_rtd10Data,std_rtd10Data,'RTD with Bridge (10dB)')
 analyze_data(ave_rtdDataVd,std_rtdDataVd,'RTD with Voltage Divider')
 analyze_data(ave_rtd10DataVd,std_rtd10DataVd,'RTD with Voltage Divider (10dB)')
-% % Lab specific values
-% temp = [0,15,30,45,60];
-% tv_3 = 3.182;
-% tv_99 = 1.96;
-
-% % Uncertainties across all measurements
-% U_adc = .5 * (20/2^16);
-% UT_acc = 1;
-% UT_res = .05;
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % Plot voltage vs temperature from bridge data
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% % Make vector of data from lab
-% voltData = [ave_rtdData,ave_rtd10Data];
-% tempData = [temp,temp];
-% std_RTD = [std_rtdData,std_rtd10Data];
-% 
-% % Perform linear regression
-% coeff = polyfit(tempData,voltData,1);
-% volt_fit = coeff(2) + coeff(1).*tempData;
-% 
-% % Standard error of the fit
-% Syx = standard_error_fit(voltData,volt_fit);
-% 
-% %%% UNCERTAINTY ANALYSIS
-% Up_fit = tv_3 * Syx;
-% Up_mean = tv_99.* (std_RTD./sqrt(100));
-% 
-% figure('Name','RTD Voltage vs Temperature - bridge')
-% plot(temp,ave_rtdData,'.','MarkerSize',12)
-% hold on
-% plot(temp,ave_rtd10Data,'.','MarkerSize',12)
-% plot(tempData,volt_fit,'r')
-% legend('Bridge RTD', '10dB Bridge RTD')
-% xlabel('Temperature ($^{\circ}C$)','Interpreter','latex')
-% ylabel('Voltage (V)')
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % Plot voltage vs temperature from voltage divider data
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% % Make vector of data from lab
-% voltDataVd = [ave_rtdDataVd,ave_rtd10DataVd];
-% std_RTDVd = [std_rtdDataVd,std_rtd10DataVd];
-% 
-% % Perform linear regression
-% coeff = polyfit(tempData,voltDataVd,1);
-% volt_fitVd = coeff(2) + coeff(1).*tempData;
-% 
-% % Standard error of the fit
-% Syx = standard_error_fit(voltDataVd,volt_fitVd);
-% 
-% figure('Name','RTD Voltage vs Temperature - voltage divider')
-% plot(temp,ave_rtdDataVd,'.','MarkerSize',12)
-% hold on
-% plot(temp,ave_rtd10DataVd,'.','MarkerSize',12)
-% plot(tempData,volt_fitVd,'r')
-% legend('Voltage Divider RTD','10dB Voltage Divider RTD')
-% xlabel('Temperature ($^{\circ}C$)','Interpreter','latex')
-% ylabel('Voltage (V)')
 
 end
 
