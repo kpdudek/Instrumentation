abeb8711d144bb68312e7e7ae5b5b5d673f04583
kpdudek <kpdudek2@gmail.com>
2019-10-31 03:24:51 -0400

    Finished

--- Lab4/Lab4.m
+++ Lab4/Lab4.m
@@ -34,7 +34,8 @@ data = read_data('cel');
 % Get averages and standard deviations
 avgData = mean(data);
 stdData = std(data);
-
+fprintf('Mean Volt: %5.3f\n',avgData)
+fprintf('STD Volt: %5.3e\n',stdData)
 % Lab specific values
 temp = [0,15,30,45,60]; % Water bath temps
 Vapplied = 1.5; %V - voltage divider applied voltage
@@ -42,7 +43,7 @@ R = 980; %ohms - Voltage divider constant resistance
 
 % Plot average voltage vs temp
 figure('Name','Thermistor Volt')
-plot(temp,avgData,'.','MarkerSize',12)
+plot(temp,avgData,'.','MarkerSize',14)
 xlabel('Temperature ($^{\circ}C)$','Interpreter','latex')
 ylabel('Voltage ($V$)','Interpreter','Latex')
 
@@ -50,22 +51,27 @@ ylabel('Voltage ($V$)','Interpreter','Latex')
 Rtherm = Volt_to_Resistance(avgData,Vapplied,R);
 Rmeas = [7.357,3.511,1.818,1.006,.561].*1000; %ohms
 
+disp('Calcualted Resistance')
+disp(Rtherm)
+disp('Measured Resistance')
+disp(Rmeas)
+
 % Plot calculated and measured resistance vs temp
 figure('Name','Thermistor Resistance')
-plot(temp,Rtherm,'.','MarkerSize',12)
+plot(temp,Rtherm,'.','MarkerSize',14)
 hold on
-plot(temp,Rmeas,'.','MarkerSize',12)
+plot(temp,Rmeas,'.','MarkerSize',14)
 xlabel('Temperature ($^{\circ}C)$','Interpreter','latex')
-ylabel('Voltage ($V$)','Interpreter','Latex')
+ylabel('Resistance ($\Omega$)','Interpreter','Latex')
 legend('Voltage Divider','Measured')
 
 % Getting data into linear form
 lnR = log(Rtherm);
 invT = 1./(temp+273); % convert temp to degrees K
 figure('Name','Ln resistance vs 1/T')
-plot(lnR,invT,'.','MarkerSize',12)
+plot(lnR,invT,'.','MarkerSize',14)
 xlabel('$ln(R)$','Interpreter','latex')
-ylabel('$\frac{1}{T}(^{\circ}C)$','Interpreter','latex')
+ylabel('$\frac{1}{T}(K)$','Interpreter','latex')
 
 end
 
@@ -123,34 +129,37 @@ Up_mean = tv_99.* (std./sqrt(100));
 U_volt = sqrt(Up_fit^2 + Up_mean.^2 + U_adc^2);
 U_temp = sqrt(UT_acc^2 + UT_res^2);
 
-uncert = sqrt((U_volt/coeff(1)).^2 + U_temp^2);
+uncert_T = sqrt((U_volt/coeff(1)).^2 + U_temp^2);
+uncert_V = sqrt((U_volt).^2 + (U_temp*coeff(1))^2);
+
 fprintf('Up Fit = %5.3e\n',Up_fit)
 fprintf('Up Mean = %5.3e\n',Up_mean)
 fprintf('Total U Voltage = %5.3e\n',U_volt)
 fprintf('Total U Temperature = %5.3e\n',U_temp)
-fprintf('Uncertainty = %5.3f\n',uncert)
+fprintf('Temp Uncertainty = %5.3f\n',uncert_T)
+fprintf('Volt Uncertainty = %5.3e\n',uncert_V)
 
 % Plot calibration
 figure('Name',iden)
-plot(temp,volt,'.','MarkerSize',12)
+plot(temp,volt,'.','MarkerSize',14)
 hold on
-plot(temp,volt_fit,'r')
+plot(temp,volt_fit,'r','LineWidth',1.5)
 legend(iden,'Linear Fit')
 xlabel('Temperature ($^{\circ}C$)','Interpreter','latex')
 ylabel('Voltage (V)')
 % xlim([min(temp)*.9 max(temp)*1.1])
 % ylim([min(volt_fit)*.9 max(volt_fit)*1.1])
 
-% Plot uncertainty
-temp_func = (volt - coeff(2)) ./ coeff(1);
-figure('Name',sprintf('%s, uncert',iden))
-errorbar(volt,temp_func,uncert,'.','MarkerSize',12)
-hold on
-legend(iden)
-ylabel('Temperature ($^{\circ}C$)','Interpreter','latex')
-xlabel('Voltage (V)')
-% xlim([min(volt)*.9 max(volt)*1.1])
-% ylim([min(temp_func)*.9 max(temp_func)*1.1])
+% % Plot uncertainty
+% temp_func = (volt - coeff(2)) ./ coeff(1);
+% figure('Name',sprintf('%s, uncert',iden))
+% errorbar(volt,temp_func,uncert,'.','MarkerSize',12)
+% hold on
+% legend(iden)
+% ylabel('Temperature ($^{\circ}C$)','Interpreter','latex')
+% xlabel('Voltage (V)')
+% % xlim([min(volt)*.9 max(volt)*1.1])
+% % ylim([min(temp_func)*.9 max(temp_func)*1.1])
 
 end
 
