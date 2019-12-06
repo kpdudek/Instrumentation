0ebca7ca9a0a8a50019ec297d076c4be80a3387c
Kurt Dudek <kpdudek@bu.edu>
2019-12-05 23:12:42 -0500

    Project code fixed

--- Project/analyzeData.m
+++ Project/analyzeData.m
@@ -3,9 +3,10 @@ close all; clc; clear;
 FreeLow = load('FreeDecay_LowerRelease.mat');
 FreeHigh = load('FreeDecay_UpperRelease.mat');
 Forced100 = load('Forced_100.mat');
+Forced12 = load('Forced_12.mat');
 
-
-analyzeSensorData(Forced100.data,'F100');
+analyzeSensorData(Forced12.data,'F12');
+% analyzeSensorData(Forced100.data,'F100');
 % analyzeSensorData(FreeLow.data,'FreeLow');
 % analyzeSensorData(FreeHigh.data,'FreeHigh');
 
--- Project/analyzeSensorData.m
+++ Project/analyzeSensorData.m
@@ -1,25 +1,49 @@
-function [PulseTime,PulseHeight,len] = analyzeSensorData(data,iden)
-figure('Name',iden)
+close all; clc; clear;
 
+load('Forced_12.mat');
+% load('Forced_100.mat');
+
+% Frequency
 fs = 1/250000;
-% time = 1:fs:length(data);
-thresh = .01;
 
-% Square the data to make all values positive and then set the noise to
-% zero
+% Square the data to make all values positive
 filtData = data.^2;
-filtData(find(filtData < thresh)) = -1;
 
-echoThresh=4;
-numberSamplesPerPulse = 9417;%1738000-1729000;
-firstEchoTime = min(find(filtData>echoThresh));
-while 1
+% Stays constant
+trigThresh = 4;
+
+% Must be adjusted based on pulse height
+echoThresh = .003;
+
+% Increase if plot is moving right
+% Decrease if plot is moving left
+% Motor Speed 12: 9426
+% Motor Speed 100: 9418
+numberSamplesPerPulse = 9426;
+
+% Arrays to store the data and a time variable to get absolute time
+time = [];
+dist = [];
+prev_time = 0;
+
+% Find the first trigger
+firstEchoTime = min(find(filtData>trigThresh));
+
+% The loop checks if the (current trigger time + pulse time) is in the
+% range of the data set
+while firstEchoTime+numberSamplesPerPulse <= length(filtData)
+% Isolate the current trigger time plus the number of samples in a pulse to work with 
 pulse=filtData(firstEchoTime:firstEchoTime+numberSamplesPerPulse);
-plot(pulse)
+
+% Uncomment for debugging the samples per pulse
+% plot(pulse)
+
+% Go through the indices after the trigger width until the value is above
+% the echo voltage threshold
 foundPulse = 0;
-sampleIndex = 200;
+sampleIndex = 500;
 while ~foundPulse
-    if(pulse(sampleIndex)>-0.5)
+    if(pulse(sampleIndex)>echoThresh)
         foundPulse=1;
     else
         sampleIndex=sampleIndex+1;
@@ -27,11 +51,52 @@ while ~foundPulse
 end
 travelTime = sampleIndex*fs;
 soundSpeed=343;
-travelDistance=travelTime/soundSpeed;
-travelDistance=(sampleIndex*fs+5.71e-4)/5.83e-3
-plot(pulse)
-firstEchoTime=firstEchoTime+numberSamplesPerPulse;
+
+% Get the travel distance two ways. Using the speed of sound and our
+% calibration
+travelDistance_SoS = travelTime*soundSpeed/2;
+travelDistance_Cal = (sampleIndex*fs+5.71e-4)/5.83e-3;
+
+% Make sure the distances arent radically different
+if abs(travelDistance_Cal-travelDistance_SoS) > .1
+    disp('FUCK')
 end
+
+% Store the absolute time, and the travel distance
+time = [time, prev_time+travelTime];
+dist = [dist, travelDistance_Cal];
+
+%%%%%% UPDATE LOOP VARIABLES %%%%%%%
+% Add the samples per pulse
+firstEchoTime = firstEchoTime+numberSamplesPerPulse;
+prev_time = prev_time+travelTime;
+end
+
+% Plot it so we can see what the last frame looks like. It should start
+% exactly at the beginning of a trigger
+figure()
+plot(pulse)
+
+
+% Plot distance vs time
+figure()
+plot(time,dist)
+xlabel('Time (s)')
+ylabel('Distance From Sensor (m)')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 % for iData = 2:length(filtData)-1
 %     if (filtData(iData)==-1) && ((filtData(iData-1)~=0) || (filtData(iData+1)~=0))
 %         filtData(iData)= mean([filtData(iData-1),filtData(iData+1)]);
